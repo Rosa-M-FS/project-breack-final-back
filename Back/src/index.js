b3e5c96412e3cf604af1c74ab88c588369bdd3e8
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-05-03 10:35:50 +0200

    dev

---
+++ Back/src/index.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const authRoutes = require("./routes/auth");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+
+
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productsRoutes);
+app.use("/api/users", usersRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Conectado a MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    });
+  })
+  .catch((err) => console.error("Error al conectar a MongoDB:", err));
