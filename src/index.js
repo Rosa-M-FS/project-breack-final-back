9bcb79fa9fcb6bf990016a623198c54f1e113549
Rosa-M-FS <rmlopezrobles@gmail.com>
2025-05-04 12:33:56 +0200

    f regist

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin:'https://rosesartesanalshop.netlify.app',
+  credentials:true
+}));
 app.use(express.json());
 
 const authRoutes = require("./routes/auth");
